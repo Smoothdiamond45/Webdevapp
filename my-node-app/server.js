00a81a7602fa72646bef16e37a3aa4ef30de3c8d
Matthew <smoothdiamond45@gmail.com>
2026-02-27 21:39:59 +0000

    test

--- my-node-app/server.js
+++ my-node-app/server.js
@@ -7,6 +7,7 @@ import { create } from 'express-handlebars';
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.static("public"));
 
 const handlebars = create({
   extname: '.hbs',
