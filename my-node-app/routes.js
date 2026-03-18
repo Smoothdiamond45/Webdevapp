006e601372571e88667a42ec554cda12fdf980e8
Matthew <smoothdiamond45@gmail.com>
2026-03-18 15:57:27 +0000

    updating web dev app lab

--- my-node-app/routes.js
+++ my-node-app/routes.js
@@ -2,13 +2,21 @@
 
 import express from 'express';
 const router = express.Router();
+import logger from "./utils/logger.js";
 
 import start from './controllers/start.js';
 import dashboard from './controllers/dashboard.js';
+import about from './controllers/about.js';
+import playlist from './controllers/playlist.js';
 
 router.get('/', start.createView);
 router.get('/dashboard', dashboard.createView);
-
+router.get('/about', about.createView);
+router.get('/playlist/:id', playlist.createView);
+router.post('/playlist/:id/addsong', playlist.addSong);
+router.post('/dashboard/addplaylist', dashboard.addPlaylist);
+router.get('/playlist/:id/deletesong/:songid', playlist.deleteSong);
 router.get('/error', (request, response) => response.status(404).end('Page not found.'));
+router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
 
 export default router;
--- my-node-app/server.js
+++ my-node-app/server.js
@@ -1,17 +1,35 @@
 'use strict';
 
 import express from 'express';
-import routes from "./routes.js";
 import logger from "./utils/logger.js";
+import routes from './routes.js'; 
 import { create } from 'express-handlebars';
+import bodyParser from "body-parser";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false, }));
 
 const handlebars = create({
-  extname: '.hbs',
-  partialsDir: 'views/partials'
+  extname: '.hbs', 
+    helpers: {
+      uppercase: (inputString) => {
+        return inputString.toUpperCase();
+      },
+     formatDate: (date) => {
+    let dateCreated = new Date(date);
+    let options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+    };
+    return `${dateCreated.toLocaleDateString("en-IE", options)}`;
+},
+
+    }
 });
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
